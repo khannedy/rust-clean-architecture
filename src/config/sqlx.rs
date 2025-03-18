27d0b2e1f118dd8646be48e5e8677cad1e1595ef
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2025-03-18 11:55:10 +0700

    Add database pool configuration using sqlx
    Introduce a `get_db_pool` function in a new `sqlx` module to establish a PostgreSQL connection pool. This includes setting parameters like max/min connections, timeouts, and the database URL.

---
+++ src/config/sqlx.rs
@@ -0,0 +1,15 @@
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+use std::time::Duration;
+
+pub async fn get_db_pool() -> Pool<Postgres> {
+    let url = "postgres://khannedy@localhost:5432/rust_clean_architecture";
+    PgPoolOptions::new()
+        .max_connections(10)
+        .min_connections(5)
+        .acquire_timeout(Duration::from_secs(5))
+        .idle_timeout(Duration::from_secs(30))
+        .connect(url)
+        .await
+        .unwrap()
+}
